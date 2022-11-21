9b79b10aa84e57e3efa1a386693bf800e1fbe222
Jorge Villarreal <jorge.vi96@gmail.com>
2022-11-21 13:51:17 -0600

    Setup dependencies

--- CApp.cpp
+++ CApp.cpp
@@ -7,7 +7,8 @@ CApp::CApp() {
 }
 
 bool CApp::OnInit() {
-    if(SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+
+    if(SDL_Init(SDL_INIT_VIDEO < 0)) { // SDL_INIT_VIDEO automatically initializes SDL_INIT_EVENTS subsystem.
         return false;
     }
 
@@ -28,5 +29,45 @@ bool CApp::OnInit() {
 }
 
 int CApp::OnExecute() {
+    SDL_Event event;
 
+    if(!OnInit()) {
+        return -1;
+    }
+
+    while (isRunning) {
+        while (SDL_PollEvent(&event) != 0) {
+            OnEvent(&event);
+        }
+        OnLoop();
+        OnRender();
+    }
+
+    return 0;
+}
+
+void CApp::OnEvent(SDL_Event *event) {
+    if (event->type == SDL_QUIT) {
+        isRunning = false;
+    }
+}
+
+void CApp::OnLoop() {
+
+}
+
+void CApp::OnRender() {
+    // Set background color to white.
+    SDL_SetRenderDrawColor(pRenderer, 255, 255, 255, 255);
+
+    // Show the result.
+    SDL_RenderPresent(pRenderer);
+}
+
+void CApp::OnExit() {
+    // Tidy up SDL2 stuff.
+    SDL_DestroyRenderer(pRenderer);
+    SDL_DestroyWindow(pWindow);
+    pWindow = nullptr;
+    pRenderer = nullptr;
 }
---
+++ main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Jorge Villarreal on 20/11/22.
+//
+
+#include "CApp.h"
+
+int main(int argc, char* argv[]) {
+    CApp theApp;
+    return theApp.OnExecute();
+}
+
